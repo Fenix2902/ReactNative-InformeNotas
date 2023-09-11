d67602694a6ecdb916a04819351a75c25662ca69
unknown <alexandercalleagudelo@gmail.com>
2023-09-11 15:28:50 -0500

    Se configura condicionales y funciones

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -4,73 +4,143 @@ import { TextInput, Button } from "react-native-paper";
 import { View, Text } from "react-native";
 import { styles } from "../../assets/styles/styles1";
 
-
 export function Formulario() {
-    //states
-    const [IdEstudiante, setIdEstudiante] = useState("");
-    const [NombreEstudiante, setNombreEstudinte] = useState("");
-    const [Asignatura, setAsignatura] = useState("");
-    const [Nota1, setNota1] = useState("");
-    const [Nota2, setNota2] = useState("");
-    const [Nota3, setNota3] = useState("");
-    const [result, setResult] = useState("");
-    return (
-        <>
-            <View>
-                <Text>REGISTRO DE NOTAS</Text>
-            </View>
-            <View
-                style={[styles.info]}
-            >
-                <TextInput label="Id. del Estudiante" style={[styles.tinputs]} />
-                <TextInput label="Nombre del Estudiante" style={styles.tinputs} />
-                <TextInput label="Asignatura" style={styles.tinputs} />
-                <TextInput label="Not. del Momento 1 (30%)" style={styles.tinputs} />
-                <TextInput label="Not. del Momento 2 (35%)" style={styles.tinputs} />
-                <TextInput label="Not. del Momento 3 (35%)" style={styles.tinputs} />
-            </View>
-            <View style={{ flexDirection: "row", marginTop: 5 }}>
-                <Button
-                    mode="contained"
-                    icon={"calculator-variant-outline"}
-                    style={styles.btn}
-                    onPress={() => console.log("click")}>
-                    Calcular
-                </Button>
-                <Button
-                    mode="contained"
-                    icon={"content-save-check-outline"}
-                    style={styles.btn}
-                    onPress={() => console.log("click")}>
-                    {" "}
-                    Guardar
-                </Button>
-            </View>
-            <View style={{ flexDirection: "row", marginTop: 5, marginBottom: 5 }}>
-                <Button
-                    mode="contained"
-                    buttonColor="red"
-                    style={styles.btn}
-                    icon="close-circle-outline"
-                    onPress={() => {
-                        setNota1("")
-                        setNota2("")
-                        setResult("")
-                    }}
-                >
-                    Limpiar
-                </Button>
-                <Button
-                    mode="contained"
-                    icon="account-search-outline"
-                    style={styles.btn}>Buscar</Button>
-            </View>
-            <View style={[styles.tinputs]}>
-                <Text>Resultado</Text>
-                <Text>Resultado</Text>
-                {/* <TextInput label="Nota Definitiva (calculado)" style={{ margin: 5 }} />
+  //states
+  const [IdEstudiante, setIdEstudiante] = useState("");
+  const [NombreEstudiante, setNombreEstudinte] = useState("");
+  const [Asignatura, setAsignatura] = useState("");
+  const [Nota1, setNota1] = useState("");
+  const [Nota2, setNota2] = useState("");
+  const [Nota3, setNota3] = useState("");
+  const [result, setResult] = useState("");
+
+  let Guardar = () => {
+    if (IdEstudiante === "" || NombreEstudiante === "" || Asignatura === "") {
+      alert("Faltan datos");
+    } else {
+      alert("datos guardados");
+    }
+  };
+  let Calcular = () => {
+    if (Nota1 === "" || Nota2 === "" || Nota3 === "") {
+      alert("faltan notas");
+    } else {
+      let nota1Valor = parseFloat(Nota1);
+      let nota2Valor = parseFloat(Nota2);
+      let nota3Valor = parseFloat(Nota3);
+
+      if (!isNaN(nota1Valor) && !isNaN(nota2Valor) && !isNaN(nota3Valor)) {
+        if (nota1Valor <= 5 && nota1Valor >= 0 && nota2Valor <= 5 && nota2Valor >=0 && nota3Valor <= 5 && nota3Valor>= 0) {
+          let promedio = (
+            nota1Valor * 0.3 +
+            nota2Valor * 0.35 +
+            nota3Valor * 0.35
+          ).toFixed(2);
+          setResult(promedio.toString());
+        } else {
+          alert("las notas deben ser numeros entre 0 y 5");
+        }
+      } else {
+        alert("los campos no pueden estar vacios");
+      }
+    }
+  };
+  return (
+    <>
+      <View>
+        <Text>REGISTRO DE NOTAS</Text>
+      </View>
+      <View style={[styles.info]}>
+        <TextInput
+          label="Id. del Estudiante"
+          onChangeText={(IdEstudiante) => setIdEstudiante(IdEstudiante)}
+          value={IdEstudiante}
+          style={[styles.tinputs]}
+        />
+        <TextInput
+          label="Nombre del Estudiante"
+          onChangeText={(NombreEstudiante) =>
+            setNombreEstudinte(NombreEstudiante)
+          }
+          value={NombreEstudiante}
+          style={styles.tinputs}
+        />
+        <TextInput
+          label="Asignatura"
+          onChangeText={(Asignatura) => setAsignatura(Asignatura)}
+          value={Asignatura}
+          style={styles.tinputs}
+        />
+        <TextInput
+          label="Not. del Momento 1 (30%)"
+          value={Nota1}
+          onChangeText={(Nota1) => setNota1(Nota1)}
+          style={styles.tinputs}
+        />
+        <TextInput
+          label="Not. del Momento 2 (35%)"
+          value={Nota2}
+          onChangeText={(Nota2) => setNota2(Nota2)}
+          style={styles.tinputs}
+        />
+        <TextInput
+          label="Not. del Momento 3 (35%)"
+          value={Nota3}
+          onChangeText={(Nota3) => setNota3(Nota3)}
+          style={styles.tinputs}
+        />
+      </View>
+      <View style={{ flexDirection: "row", marginTop: 5 }}>
+        <Button
+          mode="contained"
+          icon={"calculator-variant-outline"}
+          style={styles.btn}
+          onPress={() => Calcular()}
+        >
+          Calcular
+        </Button>
+        <Button
+          mode="contained"
+          icon={"content-save-check-outline"}
+          style={styles.btn}
+          onPress={() => Guardar()}
+        >
+          {" "}
+          Guardar
+        </Button>
+      </View>
+      <View style={{ flexDirection: "row", marginTop: 5, marginBottom: 5 }}>
+        <Button
+          mode="contained"
+          buttonColor="red"
+          style={styles.btn}
+          icon="close-circle-outline"
+          onPress={() => {
+            setIdEstudiante("");
+            setNombreEstudinte("");
+            setAsignatura("");
+            setNota1("");
+            setNota2("");
+            setNota3("");
+            setResult("");
+          }}
+        >
+          Limpiar
+        </Button>
+        <Button
+          mode="contained"
+          icon="account-search-outline"
+          style={styles.btn}
+        >
+          Buscar
+        </Button>
+      </View>
+      <View style={[styles.tinputs]}>
+        <Text>Su Promedio es de : {result}</Text>
+        {/* <Text>{result}</Text> */}
+        {/* <TextInput label="Nota Definitiva (calculado)" style={{ margin: 5 }} />
                 <TextInput label="Observaciòn (calculado)" style={{ margin: 5 }} /> */}
-            </View>
-        </>
-    );
+      </View>
+    </>
+  );
 }
